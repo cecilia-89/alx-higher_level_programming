9fb84c64db5b0eb863961877f6d4e91f2fd17883
cecilia-89 <catabong89@gmail.com>
2022-06-02 12:45:12 +0000

    initial commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,9 @@
 
 import add_0
 
-a = 1
-b = 2
+if __name__ == "__main__":
 
-print("{:d} + {:d} = {:d}".format(a, b, a + b))
+    a = 1
+    b =2
 
-print()
+    print("{:d} + {:d} = {:d}".format(a, b, a + b))
