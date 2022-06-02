d290d32c492dc9680d62ffd037732b5dd927e7c1
cecilia-89 <catabong89@gmail.com>
2022-06-02 21:21:31 +0000

    fixed bug

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import calculator_1
+from calculator_1 import add, sub, div, mul
 
 if __name__ == "__main__":
 
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} / {:d} = {:.0f}".format(a, b, div(a, b)))
