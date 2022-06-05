8e969de2c18c820f1a5226f28073fc404a130e9d
cecilia-89 <catabong89@gmail.com>
2022-06-05 21:08:17 +0000

    refactor to suite task

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,10 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
 
-    if my_list is None:
-        return
-
-    if not idx < 0 or idx > len(my_list):
+    if not (idx < 0 or idx > len(my_list)):
 
         my_list[idx] = element
 
