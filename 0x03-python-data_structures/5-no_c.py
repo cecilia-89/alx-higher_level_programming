2e963148190c6a734195ea58883914cdadbb5542
cecilia-89 <catabong89@gmail.com>
2022-06-05 21:54:41 +0000

    short fixes

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,11 @@
 
 def no_c(my_string):
 
-    for i, ele in enumerate(my_string):
+    if my_string:
 
-        if ele == 'c' or ele == 'C':
-           my_string = my_string[:i] + '' + my_string[i + 1:]
+        for i, ele in enumerate(my_string):
 
-    return my_string
+            if ele == 'c' or ele == 'C':
+                my_string = my_string[:i] + '' + my_string[i + 1:]
+
+        return my_string
