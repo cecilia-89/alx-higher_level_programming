866c0babcd63c910f03b07ca9912b0d6e8b021c9
cecilia-89 <catabong89@gmail.com>
2022-06-06 10:44:39 +0000

    corrections

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,7 +6,8 @@ def no_c(my_string):
 
         for i, ele in enumerate(my_string):
 
-            if ele == 'c' or ele == 'C':
-                my_string = my_string[:i] + '' + my_string[i + 1:]
+            if ele in 'Cc':
+                my_string[i] = ''
+                ''.join(my_string)
 
     return my_string
