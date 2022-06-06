a61a9fa33575613a82d4792aab52006054f7e710
cecilia-89 <catabong89@gmail.com>
2022-06-06 22:43:36 +0000

    added: new tasks

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if tuple_a and tuple_b:
+
+        if len(tuple_a) > 2:
+
+            tuple_a 
+
+        return ()
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    if sentence:
+
+        length = len(sentence)
+
+        return (length, sentence[0])
+
+    return(0, None)
