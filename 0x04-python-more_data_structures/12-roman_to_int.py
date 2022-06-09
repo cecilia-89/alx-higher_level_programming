8747b14963324caab0e23fa16b0315a8d0e550a6
cecilia-89 <catabong89@gmail.com>
2022-06-09 03:44:42 +0000

    attemted fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,10 +3,10 @@
 def roman_to_int(roman_string):
 
     roman = {
-     'I':1, 'IV':4,'V':5, 
-     'XI':9, 'X':10, 'XL':40,
-     'L':50, 'C':100, 'D':500, 
+     'I': 1, 'IV': 4,'V': 5, 
+     'XI': 9, 'X': 10, 'XL': 40,
+     'L': 50, 'C': 100, 'D': 500, 
      }
 
-    res = [roman[i] for i in roman_string if ]
+    res = [roman[i] for i in roman_string]
     return sum(res)
