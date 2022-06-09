3d19355571ca5572cb188bdc1b9c8550ea398f1d
cecilia-89 <catabong89@gmail.com>
2022-06-09 03:09:26 +0000

    attempted fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,16 +2,15 @@
 
 def search_replace(my_list, search, replace):
 
-    if my_list:
 
-        new_list = []
+    new_list = []
 
-        for idx in my_list:
+    for idx in my_list:
 
-            if idx == search:
-                new_list.append(replace)
+        if idx == search:
+            new_list.append(replace)
 
-            else:
-                new_list.append(idx)
+        else:
+            new_list.append(idx)
 
-        return(new_list)
+    return(new_list)
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+
+    roman = {
+     'I':1, 'IV':4,'V':5, 
+     'XI':9, 'X':10, 'XL':40,
+     'L':50, 'C':100, 'D':500, 
+     }
+
+    res = [roman[i] for i in roman_string if ]
+    return sum(res)
