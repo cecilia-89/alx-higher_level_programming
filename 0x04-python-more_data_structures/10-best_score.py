f2786e170853db1faee86b35e1aa7e07ee052bc1
cecilia-89 <catabong89@gmail.com>
2022-06-09 03:37:29 +0000

    attempted fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,6 @@
 def best_score(a_dictionary):
 
     if a_dictionary:
-        max_num = list(a_dictionary.values())[0]
-
-        for key, value in a_dictionary.items():
-            if value > max_num:
-                name, max_num = key, value
-
-        return name
+        
+        res = max(a_dictionary, key=a_dictionary.get)
+        return res
