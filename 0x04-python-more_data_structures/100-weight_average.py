3c63a4b23f3f2e8b945a88c2a2620ea352fc53d6
cecilia-89 <catabong89@gmail.com>
2022-06-09 05:03:48 +0000

    advanced task

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+
+    res = [product(tuples) for tuples in my_list]
+    return(res)
---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return list(map(lambda list(map(lambda x: x**2 ))))
+
