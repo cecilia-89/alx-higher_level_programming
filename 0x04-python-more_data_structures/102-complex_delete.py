ce0b9a896355b7cf3c7be80c37ce5c86e177f22f
cecilia-89 <catabong89@gmail.com>
2022-06-09 05:05:48 +0000

    advanced task

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,10 +2,10 @@
 
 def complex_delete(a_dictionary, value):
 
-    for k,v in list(a_dictionary.items()):
+    for k, v in list(a_dictionary.items()):
 
         if v == value:
-        
+
             a_dictionary.pop(k)
 
     return a_dictionary
