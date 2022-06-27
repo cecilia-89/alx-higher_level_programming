5186e94e94db3e1708aa0d174d7498f5e29cf369
cecilia-89 <catabong89@gmail.com>
2022-06-27 13:58:06 +0000

    updated tasks

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,9 +11,9 @@ class Rectangle:
 
         """A class method called when an instance is created"""
 
-        self.__height = height
-
         self.__width = width
+
+        self.__height = height
 
     @property
     def width(self):
