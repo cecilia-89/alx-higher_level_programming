58960a4bc7317d3c0a3d95a6445d3c5dba15ebe0
cecilia-89 <catabong89@gmail.com>
2022-06-27 20:32:48 +0000

    initial commit

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp, *new;
+
+	if (*head == NULL)
+		exit(EXIT_FAILURE);
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		if (number <= temp->n)
+		{
+			new = malloc(sizeof(listint_t));
+
+			new->next = temp-next;
+
+			temp->next = new;
+
+			break;
+		}
+
+		temp = temp->next;
+	}
+
+	return (*head);
+}
