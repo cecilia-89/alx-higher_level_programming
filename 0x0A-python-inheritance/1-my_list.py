22ac48e4f088d543817ede362b40501a3656be34
cecilia-89 <catabong89@gmail.com>
2022-07-05 04:57:47 +0000

    second commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,6 +2,16 @@
 
 class MyList(list):
 
+
+    """
+    MyList: inherits from the 
+    the class list
+    """
+
     def print_sorted(self):
+
+        """
+        returns a sorted list
+        """
 
         print(sorted(self)
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
+    """
+    determines if a particular object
+    belongs to a certain class
+    """
 
     if isinstance(obj, a_class):
 
