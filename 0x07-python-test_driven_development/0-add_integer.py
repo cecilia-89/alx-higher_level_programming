ff009f0c320f58273ba41f7af3a4483391cf5d31
cecilia-89 <catabong89@gmail.com>
2022-07-07 18:53:50 +0000

    initial commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Module: 0-add_integer
+Function: add_interger(), takes two positional
+arguments a and b
+"""
+
+def add_integer(a, b=98):
+
+    """Returns the sum of two numbers"""
+
+    if type(a) != int and float:
+
+        raise TypeError("a must be an integer")
+
+    if type(b) != int and float:
+
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
