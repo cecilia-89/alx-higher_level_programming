54774d491c4a72df77c19555653046aad043f932
cecilia-89 <catabong89@gmail.com>
2022-07-08 20:34:01 +0000

    fourth commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module: 4-print_square
+Function: def print_square()
+"""
+
+def print_square(size):
+
+    if isinstance(size, float) and size < 0 or not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    [print('#' * size) for count in range(size)]
