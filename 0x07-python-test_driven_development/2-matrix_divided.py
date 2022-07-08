e135af19c5960986db0471c7d85c2452de8fac68
cecilia-89 <catabong89@gmail.com>
2022-07-08 07:17:54 +0000

    second commit

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+module: 2-matrix_divided
+function: matrix_divided()
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Returns a new matrix with each
+    elemeny divided by 'div'
+    """
+
+    length = len(matrix[0])
+
+    new_list = []
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    elif not isinstance(div, (int, float)):
+        raise TypeError("div must be a num")
+
+    for i, lst in enumerate(matrix):
+
+        if len(lst) != length:
+            raise TypeError("Each row of the matrix must have the same size")
+
+        new_list.append([])
+
+        for item in lst:
+
+            if not isinstance(item, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+            res = round(item/div, 2)
+            new_list[i].append(res)
+
+    return new_list
