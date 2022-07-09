645a404cb48fb6d9fa1d961b18c4f0db3c5b873c
cecilia-89 <catabong89@gmail.com>
2022-07-09 23:55:12 +0000

    another commit

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+is_same_class: returns true if
+an object belongs to a particular class
+else returns false
+"""
+
+
+def is_same_class(obj, a_class):
+
+    """
+    determines if a particular object
+    belongs to a certain class
+    """
+
+    if isinstance(obj, a_class):
+
+        return True
+
+    return False
