178b328470de28a9fd63fc71a56335d75c0ad723
cecilia-89 <catabong89@gmail.com>
2022-07-09 05:52:25 +0000

    fifth commit

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,7 +4,12 @@
    Function: def text_indentation(text)
 """
 
+
 def text_indentation(text):
+    """
+    Prints string with two additional lines
+    after a set of symbols
+    """
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
@@ -18,6 +23,6 @@ def text_indentation(text):
         if char == " " and text[count-1] in symbols:
             continue
 
-        print(char, end="" if not res else "\n\n")
+        print(char, end="" if not res else "\n")
 
     print()
