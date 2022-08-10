3de209e6a99c5ab3a038f17f596b48010039a584
cecilia-89 <catabong89@gmail.com>
2022-08-10 23:39:00 +0100

    added: Read me file

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,5 @@
 -- Creates new table
-CREATE TABLE IF NOT EXISTS 'id_not_null' (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS 'id_not_null' (
+	id INT DEFAULT 1,
+	name VARCHAR(256)
+	);
