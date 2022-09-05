492725ee226ee4cd054cabda76ae2ed5908c367f
cecilia-89 <catabong89@gmail.com>
2022-09-06 00:29:05 +0100

    added new tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ let res = 0;
 const args = process.argv.slice(2);
 
 if (args.length > 1) {
-  res = args.sort(function(a, b) {return b - a})[1];
+  res = args.sort(function (a, b) { return b - a; })[1];
 }
 console.log(res);
