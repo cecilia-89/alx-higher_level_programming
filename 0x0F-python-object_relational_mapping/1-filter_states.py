62713a7d8ede362cdefa58e6ae56624b0fb38f69
cecilia-89 <catabong89@gmail.com>
2022-09-10 15:59:15 +0100

    refactor to fit pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,12 +4,15 @@ Module: 1-filter_states.py
 lists all states from the database hbtn_0e_0_usa
 """
 if __name__ == '__main__':
-	import MySQLdb, sys
-	db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
-	queries = cur.fetchall()
-	for query in queries:
-		print(query)
-	cur.close()
-	db.close()
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name \
+                LIKE 'N%' ORDER BY states.id ASC")
+    queries = cur.fetchall()
+    for query in queries:
+        print(query)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,12 +4,15 @@ Module: 2-my_filter_states.py
 lists all states from the database hbtn_0e_0_usa
 """
 if __name__ == '__main__':
-	import MySQLdb, sys
-	db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-	cur = db.cursor()
-	cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(sys.argv[4]))
-	queries = cur.fetchall()
-	for query in queries:
-		print(query)
-	cur.close()
-	db.close()
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' \
+                ORDER BY states.id ASC".format(sys.argv[4]))
+    queries = cur.fetchall()
+    for query in queries:
+        print(query)
+    cur.close()
+    db.close()
