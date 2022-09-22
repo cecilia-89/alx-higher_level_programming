69a22fcc7ae6eb67299f4a54ee9ddcf9c85c0fd2
cecilia-89 <catabong89@gmail.com>
2022-09-22 19:40:08 +0100

    added new line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #displays the size of the body of the response
-
 curl -s -i $1 | grep Content-Length | tail -c 4
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #displays the size of the body of the response
-#depending on the status code
-
 curl -Ls $1
