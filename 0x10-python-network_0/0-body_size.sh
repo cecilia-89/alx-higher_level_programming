0bdd7a0b7930f11634f2a2d11c98c1e9cbd419fe
cecilia-89 <catabong89@gmail.com>
2022-09-22 19:37:12 +0100

    initial commit

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#displays the size of the body of the response
+
+curl -s -i $1 | grep Content-Length | tail -c 4
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#displays the size of the body of the response
+#depending on the status code
+
+curl -Ls $1
