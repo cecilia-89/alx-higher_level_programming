90fec311469d618c5b3af4fb002b2dc86ac57954
cecilia-89 <catabong89@gmail.com>
2022-09-23 23:52:24 +0100

    initial commit

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,7 @@ from urllib import request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with request.urlopen(url) as resp:
-        print(f"Body response:\n\t- type: {resp.peek().__class__}",
-              f"\n\t- content: {resp.read()}\n\t- utf8 content: {resp.reason}")
+    with request.urlopen(url) as r:
+        print(f"Body response:\n\t- type:{r.peek().__class__}\
+              \n\t- content: {r.read()}\
+              \n\t- utf8 content: {r.reason}".expandtabs(4))
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,7 @@ from urllib import request
 from sys import argv
 
 if __name__ == "__main__":
-    url = f"{argv[1]}?email={argv[2]}"
+    url ="{}?email={}".format(argv[1], argv[2])
     with request.urlopen(url) as resp:
         data = resp.read()
         print(data.decode('utf-8'))
