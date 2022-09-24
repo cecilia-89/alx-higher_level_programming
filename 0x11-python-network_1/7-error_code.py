da0af204db0156bb5125de39e656f68b9b15dd7e
cecilia-89 <catabong89@gmail.com>
2022-09-24 22:11:39 +0100

    added tasks

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     r = requests.get(argv[1])
     if r.status_code >= 400:
-        print(f"Error code: {r.status_code")
+        print(f"Error code: {r.status_code}")
     else:
         print(r.text)
 
