3904bddc34ff7d8a8ccaa87177010216d2675de8
cecilia-89 <catabong89@gmail.com>
2022-09-24 22:06:15 +0100

    added tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,6 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get("https://alx-intranet.hbtn.io/status")
-    print(f"Body response:\n\t- type: {type(r.text)}\
-          \n\t- content: {r.text}".expandtabs(4))
+    print("Body response:")
+    print("\t- type:", type(r.text))
+    print("\t- content:", r.text)
