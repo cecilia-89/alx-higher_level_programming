96dc7263f4d062d23d8a50dcd14d8c37c9e4b11d
cecilia-89 <catabong89@gmail.com>
2022-09-24 07:58:48 +0100

    added tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
     with request.urlopen(url) as r:
         data = r.read()
-        print(f"Body response:\n- type:{data.__class__}\n- content: {data}\
-              \n- utf8 content: {data.decode('utf-8')}")
+        print(f"Body response:\n\t- type:{data.__class__}\n\t- content: {data}",
+              f"\n\t- utf8 content: {data.decode('utf-8')}")
