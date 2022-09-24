af660f7c21c73ee305854ffbf2fddff9521b2d6e
cecilia-89 <catabong89@gmail.com>
2022-09-24 22:09:16 +0100

    added tasks

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     param = {'email': argv[2]}
     query_string = parse.urlencode(param)
-    url = f"{argv[1]}?{query_string}
+    url = f"{argv[1]}?{query_string}"
     with request.urlopen(url) as resp:
         data = resp.read()
         print(data.decode('utf-8'))
