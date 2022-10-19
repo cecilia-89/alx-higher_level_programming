76a53635220ad546edc93b3652f460701e9e3634
cecilia-89 <catabong89@gmail.com>
2022-10-19 21:20:10 +0100

    initial commit

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,21 +6,18 @@ request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   }
-  const ep = JSON.parse(body)
-  displayChar(ep.characters, 0)
-
+  const ep = JSON.parse(body);
+  displayChar(ep.characters, 0);
 });
 
-function displayChar(chars, count) {
+function displayChar (chars, count) {
   request.get(chars[count], (err, res, body) => {
     if (!err) {
       console.log(JSON.parse(body).name);
 
-      if (count++ < chars.length){
-        displayChar(chars, count)
+      if (count++ < chars.length) {
+        displayChar(chars, count);
       }
     }
-
-  })
-
+  });
 }
